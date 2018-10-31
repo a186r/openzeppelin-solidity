8391493343b702c2b47a1c29c1061ef6ccb82501
a186 <hi.aewalle@gmail.com>
2018-10-31 15:13:05 +0800

    commit

--- contracts/examples/SimpleToken.sol
+++ contracts/examples/SimpleToken.sol
@@ -10,17 +10,17 @@ import "../token/ERC20/ERC20.sol";
  */
 contract SimpleToken is ERC20 {
 
-  string public constant name = "SimpleToken";
-  string public constant symbol = "SIM";
-  uint8 public constant decimals = 18;
+    string public constant name = "SimpleToken";
+    string public constant symbol = "SIM";
+    uint8 public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
-  constructor() public {
-    _mint(msg.sender, INITIAL_SUPPLY);
-  }
+    /**
+    * @dev Constructor that gives msg.sender all of existing tokens.
+    */
+    constructor() public {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
 
 }
--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -12,17 +12,17 @@ library Address {
    * @param account address of the account to check
    * @return whether the target address is a contract
    */
-  function isContract(address account) internal view returns (bool) {
-    uint256 size;
-    // XXX Currently there is no better way to check if there is a contract in an address
-    // than to check the size of the code at that address.
-    // See https://ethereum.stackexchange.com/a/14016/36603
-    // for more details about how this works.
-    // TODO Check this again before the Serenity release, because all addresses will be
-    // contracts then.
-    // solium-disable-next-line security/no-inline-assembly
-    assembly { size := extcodesize(account) }
-    return size > 0;
-  }
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        // XXX Currently there is no better way to check if there is a contract in an address
+        // than to check the size of the code at that address.
+        // See https://ethereum.stackexchange.com/a/14016/36603
+        // for more details about how this works.
+        // TODO Check this again before the Serenity release, because all addresses will be
+        // contracts then.
+        // solium-disable-next-line security/no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
 
 }
